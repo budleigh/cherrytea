507c061a09c4112cbe5e2377e37c3979471ca261
Sam Wainwright <arguewithplato@gmail.com>
2018-02-10 23:17:49 -0800

    add static root

--- cherrytea/settings.py
+++ cherrytea/settings.py
@@ -133,6 +133,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+# misc
+
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
 LOGIN_URL = ''

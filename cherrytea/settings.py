c06f702aefec2fd8eae2bf676e0e146fc2b3a167
Sam Wainwright <arguewithplato@gmail.com>
2018-02-10 23:12:29 -0800

    update for deploy ready

--- cherrytea/settings.py
+++ cherrytea/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -85,6 +87,10 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+if os.environ.get('DATABASE_URL'):
+    db_from_env = dj_database_url.config()
+    DATABASES['default'].update(db_from_env)
 
 
 # Password validation
